55e540eb0fa890d31f4d6c3e43b46ba6b46961b4
Savvin Maksim <40721470+savvinm@users.noreply.github.com>
2022-05-28 22:12:51 +0300

    Initial Commit

---
+++ IMDb Movies/IMDb_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDb_MoviesApp.swift
+//  IMDb Movies
+//
+//  Created by Maksim Savvin on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct IMDb_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
